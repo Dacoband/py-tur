226f9a387c636d51b7e194e22ee22374a42b3ba2
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-04-15 20:54:17 +0700

    practice

---
+++ Practice1/AREA_CALC.py
@@ -0,0 +1,15 @@
+
+
+
+
+length = float(input("Enter the length: "))
+
+width = float(input("Enter the width: "))
+
+height = float(input("Enter the height: "))
+
+volume = length * width * height
+area = length * width
+
+print(f"The area is: {area}cm^2")
+print(f"The volume is: {volume}cm^3")
