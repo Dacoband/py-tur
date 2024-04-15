3b81252f533efdbc337192122b290b6b89b9de54
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-04-15 20:00:28 +0700

    Variable

---
+++ Variable/main.py
@@ -0,0 +1,61 @@
+# variable = a reusable container fo storing a value
+
+age = 30
+name = "Volka"
+job = "Software Engineer"
+dateOfBirth = "28/09/1990"
+
+# 1
+print("I'm", name , "age", age, "and", job)
+
+# 2
+print("I'm "+ str(name) + " age "+ str(age)+" and "+ str(job))
+
+# 3 : f:string most popular doing
+print(f"I'm {name}, age {age} and {job}")
+
+################################################################
+
+# INTEGER
+age1 = 30
+teamdeveloper = 3
+subs = 5
+
+print(f"You are {age1} years old")
+print(f"Your team dev is {teamdeveloper}")
+print(f"Your birthday is {dateOfBirth}")
+
+# FLOAT
+gpa = 3.8
+distance = 2.5
+price = 18.99
+
+print(f"Your GPA is {gpa}")
+print(f"Your ran {distance}km")
+print(f"The price is {price}")
+
+# STRING
+
+name = "Volka"
+job = "Software Engineer"
+
+print(f"I'm {name}, age {age} and {job}")
+
+# BOOLEAN (true || false)
+
+online = True
+for_sale = False
+running =True
+
+print(f"Are you online: {online}")
+print(f"Is the item for sale? {for_sale}")
+print(f"Game run: {running}")
+
+# Others
+
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+
+
+
